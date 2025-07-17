4f285557689a925d7ca47d0869dbdc5c1f3cfe39
Marvin Nitschke <marvin.nitschke@gmail.com>
2025-07-17 12:14:33 +0200

    adding nvim conf to repo

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,11 @@
+return {
+  -- GitHub Copilot
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
+}
