46938f7f6d154e2aafcc4c142d227799891f4a94
Marvin Nitschke <marvin.nitschke@gmail.com>
2025-07-21 17:17:04 +0200

    Changing Kitty config for new terminal window in same path.

--- ml4w/settings/terminal-here.sh
+++ ml4w/settings/terminal-here.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Get the focused window's working directory and open kitty there
-kitty --working-directory="$(hyprctl activewindow -j | jq -r '.pid' | xargs pwdx | cut -d' ' -f2-)" &
+hyprctl activewindow
